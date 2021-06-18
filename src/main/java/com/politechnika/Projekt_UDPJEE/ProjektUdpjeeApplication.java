fbdf1668e0df638d485f3d5bcd7b289846462431
Bartek_Rozga <bartek130@gmail.com>
2021-06-18 11:49:00 +0200

    Initial Commit

---
+++ src/main/java/com/politechnika/Projekt_UDPJEE/ProjektUdpjeeApplication.java
@@ -0,0 +1,13 @@
+package com.politechnika.Projekt_UDPJEE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektUdpjeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektUdpjeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/politechnika/Projekt_UDPJEE/ProjektUdpjeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politechnika.Projekt_UDPJEE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektUdpjeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
